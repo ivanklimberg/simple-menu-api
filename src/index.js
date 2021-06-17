93d00ea7d8ba5dc200d3ff1f609517817ce3a85c
iklimberg <iklimberg@IOLNOTE-ITIK>
2021-06-17 00:55:43 -0300

    adding cors configuration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import compression from 'compression';
+import cors from 'cors';
 import { urlencoded, json } from 'body-parser';
 import routes from './routes';
 
 const PORT = 9000;
 const app = express();
 
+var corsOptions = {
+    origin: 'http://localhost:3000'
+}
+
+app.use(cors(corsOptions));
 app.use(urlencoded({ extended: false }))
 app.use(json())
 app.use(compression());
 app.use(routes);
+
+
 
 app.listen(PORT, () => {
     console.log('up and running!');
