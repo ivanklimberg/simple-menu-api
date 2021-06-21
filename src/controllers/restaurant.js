41d42b1a829a9a19934ed45996dbc529cd551056
ivanklimberg <ivanklimberg@gmail.com>
2021-06-21 13:01:02 -0300

    adding total pages and moving show only with menu to api

--- src/controllers/restaurant.js
+++ src/controllers/restaurant.js
@@ -2,6 +2,13 @@ import { getFile, getAllFiles } from '../helpers/fileHelper';
 
 const PAGE_SIZE = 10;
 
+const filterRestaurants = (restaurants, showOnlyWithMenus) => {
+    return restaurants.filter(item => {
+        if(!showOnlyWithMenus) return true;
+        return item.menus.length;
+    })
+}
+
 export const getRestaurants = (req, res) => {
     try {
         if(req.params.id) {
@@ -11,10 +18,16 @@ export const getRestaurants = (req, res) => {
             
             return res.send(restaurant);
         }
+
         const page = parseInt(req.query.page || 1);
-        const restaurants = getAllFiles();
-    
+        let restaurants = getAllFiles();
+
+        if(req.query.showOnlyWithMenu) {
+            restaurants = filterRestaurants(restaurants, req.query.showOnlyWithMenu);
+        }
+        
         return res.send({
+            totalPages: Math.ceil(restaurants.length / PAGE_SIZE),
             page,
             results: restaurants.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE)
         });
